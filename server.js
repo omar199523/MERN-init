540f70dd27eba10c67b45cfad33b1de6178e0e4c
omar199523 <model19952323@gmail.com>
2021-11-12 20:46:28 +0200

    add heroku sitting

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path')
+
 const items =require('./route/api/items')
 const app = express();
 
@@ -20,6 +22,14 @@ mongoose.connect(mongoURI ,{useNewUrlParser:true,useUnifiedTopology:true})
 // });
 // use route
 app.use('/api/items',items);
+
+//servestatic assets if in production
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static('client/build'))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port ,()=>console.log(`server conect sucssceful in port ${port} `))
