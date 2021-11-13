45e5f250f88aba802d650f7522170e5cf7eef7b1
omar199523 <model19952323@gmail.com>
2021-11-13 19:46:54 +0200

    edit data base setting

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.use(bodyParser.json());
 
 
 // Databease config 
-const mongoURI = require('./config/keys').mongoURI
 
 
-mongoose.connect(  process.env.MONGODB_URI ||mongoURI ,{useNewUrlParser:true,useUnifiedTopology:true})
+// process.env.MONGODB_URI ||
+mongoose.connect( process.env.MONGODB_URI || require('./config/keys').mongoURI ,{useNewUrlParser:true,useUnifiedTopology:true})
 
 
 
@@ -30,7 +30,7 @@ app.use('/api/items',items);
 if(process.env.NODE_ENV ==='peoduction'){
     app.use(express.static('client/build'))
     app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','buildindex.html'))
     })
 }
 
