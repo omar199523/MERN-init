109fbbdf56ba5464a3457e20da88434970cbcc17
omar199523 <model19952323@gmail.com>
2021-11-13 20:18:39 +0200

    edit index.html path in server.js

--- server.js
+++ server.js
@@ -28,11 +28,14 @@ app.use('/api/items',items);
 
 //serve static assets if in production
 if(process.env.NODE_ENV ==='peoduction'){
-    app.use(express.static(path.join(__dirname,'client','build')))
-    app.get('/*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
+    app.use(express.static(path.join(__dirname, 'client','build')));
+
+    app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+    });
 }
+ 
+
 
 const port = process.env.PORT || 5000;
 app.listen(port ,()=>console.log(`server conect sucssceful in port ${port} `))
